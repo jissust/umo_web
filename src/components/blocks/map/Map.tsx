2f6aaf40f53f11122795a28f4e12c8769fab6e11
jesustissera <jesustissera@gmail.com>
2026-04-17 11:43:32 -0300

    refactor: Agregar dirección de empresa.

--- src/components/blocks/map/Map.tsx
+++ src/components/blocks/map/Map.tsx
@@ -3,7 +3,7 @@ export const Map = () => {
     <section className="w-full">
       <div className="w-full h-[400px]">
         <iframe 
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14809273.094948573!2d-6.971336949999999!3d35.79275935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc42e3783261bc8b%3A0xa6ec2c940768a3ec!2sSpain!5e1!3m2!1sen!2sar!4v1776086832284!5m2!1sen!2sar" 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1997.049934671667!2d3.220689560282923!3d39.371509742483575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x129650d9a23c98fd%3A0x51f0f9bef847934b!2sCalle%20de%20Sa%20Fonteta%2C%2011%2C%2007660%20Cala%20d&#39;Or%2C%20Illes%20Balears%2C%20Spain!5e1!3m2!1sen!2sar!4v1776436938036!5m2!1sen!2sar" 
         className="w-full h-full border-0" 
         loading="lazy" 
         referrerPolicy="no-referrer-when-downgrade"
