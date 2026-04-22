06f4c19f1ed8f11ea05f12af9ac6c67e1689acd8
jesustissera <jesustissera@gmail.com>
2026-04-22 17:36:31 -0300

    Refactor: Cambiar estructura de columnas

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 // src/components/layout/Footer.tsx
 
 import Link from "next/link";
-import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FaInstagram, FaWhatsapp, FaPhone, FaMailBulk } from "react-icons/fa";
 import { NavLinks } from "@/components/ui/navigation/NavLinks";
 import Image from "next/image";
 
@@ -9,11 +9,15 @@ export const Footer = () => {
   return (
     <footer className="w-full bg-black text-white">
       <div className="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-3 gap-8 items-center">
-        <div className="flex justify-center md:justify-start h-full">
+        <div className="flex flex-col items-center text-lg  gap-2">
+          <NavLinks />
+        </div>
+
+        <div className="flex justify-center h-full">
           <h2 className="text-2xl font-bold">
             <Link href="/" className="hover:text-gray-300">
               <Image
-                src="/img/umo_logo_593x707.png"
+                src="/img/logo_blanco_554x718.png"
                 alt="UMO logo"
                 width={200}
                 height={200}
@@ -24,27 +28,44 @@ export const Footer = () => {
           </h2>
         </div>
 
-        <div className="flex flex-col items-center gap-2">
-          <NavLinks />
-        </div>
-
-        <div className="flex justify-center md:justify-end gap-6 text-xl">
-          <a
-            href="https://www.instagram.com/umo.calador26"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-gray-300"
-          >
-            <FaInstagram />
-          </a>
-          <a
-            href="https://wa.me/34641467880"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-gray-300"
-          >
-            <FaWhatsapp />
-          </a>
+        <div>
+          <h2 className="pb-4 font-semibold text-lg text-center">Contacto</h2>
+          <div className="flex gap-4 text-center pb-8 justify-center">
+            <a
+              href="https://www.instagram.com/umo.calador26"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300 text-xl"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://wa.me/34641467880"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300 text-xl"
+            >
+              <FaWhatsapp />
+            </a>
+            <a
+              href="tel:+34871612605"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300"
+            >
+              <FaPhone />
+            </a>
+            <a
+              href="mailto:umo.calador@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300 text-xl"
+            >
+              <FaMailBulk />
+            </a>
+          </div>
+          <h2 className="pt-4 font-semibold text-lg text-center border-t-1">Ubicación</h2>
+          <p className="text-sm text-center"><b>UMO FIRE STEAKHOUSE CALA D'OR</b> <br />Calle de Sa Fonteta, 11, 07660 Cala d'Or, Balearic Islands</p>
         </div>
       </div>
     </footer>
