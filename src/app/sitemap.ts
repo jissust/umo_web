9c2e4bcd4985dead5ab840b367dde0fbaa5ed691
jesustissera <jesustissera@gmail.com>
2026-04-24 11:16:24 -0300

    refactor: agrego sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://umocalador.es" },
+    { url: "https://umocalador.es/carta" },
+    { url: "https://umocalador.es/contacto" },
+    { url: "https://umocalador.es/noticias" },
+  ];
+}
