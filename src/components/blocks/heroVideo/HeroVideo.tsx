fe8c407150176c857b84dc49adbda20defa7f2ae
jesustissera <jesustissera@gmail.com>
2026-05-13 09:33:43 -0300

    refactor: parametrizar direccion de video y agregar componente scroll

--- src/components/blocks/heroVideo/HeroVideo.tsx
+++ src/components/blocks/heroVideo/HeroVideo.tsx
@@ -2,14 +2,19 @@
 import { ButtonLink } from "@/components/ui/buttonLink/ButtonLink";
 import Image from "next/image";
 import { useLoading } from "@/context/LoadingContext";
+import { ScrollIndicator } from "@/components/ui/scrolledIndicator";
 
 type Props = {
   translations: any;
+  videoSrc: string;
+  showLogo: boolean;
 };
 
-export const HeroVideo = ({ translations }: Props) => {
+export const HeroVideo = ({ translations, videoSrc, showLogo }: Props) => {
   const { setIsLoading } = useLoading();
-  
+  //const [logo, setLogo] = useState(true);
+
+  console.log(videoSrc);
   return (
     <section className="relative w-full h-[100vh]">
       <video
@@ -20,46 +25,47 @@ export const HeroVideo = ({ translations }: Props) => {
         onLoadedData={() => setIsLoading(false)}
         className="absolute inset-0 w-full h-full object-cover"
       >
-        <source src="/hero_video.mp4" type="video/mp4" />
+        <source src={videoSrc} type="video/mp4" />
       </video>
 
       <div className="absolute inset-0 bg-black/60" />
-
-      <div
-        className="relative z-10 flex items-center h-full"
-        data-aos="fade-zoom-in"
-        data-aos-offset="300"
-        data-aos-easing="ease-in-back"
-      >
-        <div className="max-w-7xl mx-auto px-6 w-full flex-col items-center gap-6 text-center">
-          <Image
-            src="/img/umo_logo_652x883.png"
-            alt="UMO logo"
-            width={350}
-            height={350}
-            priority
-            style={{ display: "block", margin: "0 auto" }}
-          />
-          <ButtonLink
-            href="https://wa.me/34641467880"
-            bgColor="bg-gradient-to-b from-[#F1DF7D] to-[#E19025]"
-            hoverBgColor="hover:bg-none hover:bg-transparent"
-            textColor="text-black"
-            hoverTextColor="hover:text-white"
-            borderColor="border border-[#F1DF7D] border-2"
-            hoverBorderColor="hover:border-[#E19025] hover:border-2"
-            target="_blank"
-            eventData={{
-              event: "contact_click",
-              method: "whatsapp",
-              location: "home",
-            }}
-          >
-            {translations.home.hero.text_button} →
-          </ButtonLink>
+      {showLogo && (
+        <div
+          className="relative z-10 flex items-center h-full"
+          data-aos="fade-zoom-in"
+          data-aos-offset="300"
+          data-aos-easing="ease-in-back"
+        >
+          <div className="max-w-7xl mx-auto px-6 w-full flex-col items-center gap-6 text-center">
+            <Image
+              src="/img/umo_logo_652x883.png"
+              alt="UMO logo"
+              width={350}
+              height={350}
+              priority
+              style={{ display: "block", margin: "0 auto" }}
+            />
+            <ButtonLink
+              href="https://wa.me/34641467880"
+              bgColor="bg-gradient-to-b from-[#F1DF7D] to-[#E19025]"
+              hoverBgColor="hover:bg-none hover:bg-transparent"
+              textColor="text-black"
+              hoverTextColor="hover:text-white"
+              borderColor="border border-[#F1DF7D] border-2"
+              hoverBorderColor="hover:border-[#E19025] hover:border-2"
+              target="_blank"
+              eventData={{
+                event: "contact_click",
+                method: "whatsapp",
+                location: "home",
+              }}
+            >
+              {translations.home.hero.text_button} →
+            </ButtonLink>
+          </div>
         </div>
-      </div>
-      
+      )}
+      <ScrollIndicator />
     </section>
   );
 };
